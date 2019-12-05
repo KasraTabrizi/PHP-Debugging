92ad22ec064f4315930b0018c7eac36e26df9aaf
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-12-05 11:31:44 +0100

    solved exercise 6

--- expert.php
+++ expert.php
@@ -60,3 +60,65 @@ foreach($week as &$day) {
 
 print_r($week);
 /*****************************************************************************************/
+new_exercise(5);
+// === Exercise 5 ===
+// The array should be printing every letter of the alfabet (a-z) but instead it does that + aa-yz
+// Fix the code so the for loop only pushes a-z in the array
+// === PROBLEM ===
+// in php if you increment z, it will become aa, etc
+// === SOLUTION ===
+// stop the for loop when the length of the arrat is 26
+$arr = [];
+for ($letter = 'a'; count($arr) < 26; $letter++) {
+    array_push($arr, $letter);
+}
+
+print_r($arr); // Array ([0] => a, [1] => b, [2] => c, ...) a-z alfabetical array
+/*****************************************************************************************/
+new_exercise(6);
+// === Final exercise ===
+// The fixed code should echo the following at the bottom:
+// Here is the name: $name - $name2
+// $name variables are decided as seen in the code, fix all the bugs whilst keeping the functionality!
+// === PROBLEM ===
+// line 111 has a missing semicolon
+// line 114 the count of heroes is 2 but if you put that in rand it will generatre a random number between 0 and 2(included).
+// === SOLUTION ===
+// for line 111 en line 1444 see above
+// line 93 needed a & before $param
+// also added $nameOrder so it first takes a random firstname and then a random lastname
+$arr = [];
+
+function combineNames($str1 = "", $str2 = "") {
+    $params = [$str1, $str2];
+    $nameOrder = 0;
+    foreach($params as &$param) {
+        if ($param == "") {
+            $param = randomHeroName($nameOrder);
+        }
+        $nameOrder++;
+    }
+    return implode(" - ", $params);
+}
+
+
+function randomGenerate($arr, $amount) {
+    for ($i = $amount; $i > 0; $i--) {
+        array_push($arr, randomHeroName());
+    }
+
+    return $amount;
+}
+
+function randomHeroName($nameOrder)
+{
+    $hero_firstnames = ["captain", "doctor", "iron", "Hank", "ant", "Wasp", "the", "Hawk", "Spider", "Black", "Carol"];
+    $hero_lastnames = ["America", "Strange", "man", "Pym", "girl", "hulk", "eye", "widow", "panther", "daredevil", "marvel"];
+    $heroes = [$hero_firstnames, $hero_lastnames];
+    $randname = $heroes[$nameOrder][rand(0, 10)];
+
+    return $randname;
+}
+
+echo "Here is the name: " . combineNames();
+/*****************************************************************************************/
