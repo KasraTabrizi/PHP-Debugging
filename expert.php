508c72eaa9109486ca6e4c4f360222e95d905325
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-12-05 09:43:57 +0100

    solved exercise 2

--- expert.php
+++ expert.php
@@ -6,8 +6,9 @@ declare(strict_types=1);
 // Look at the error you get, read it and it should tell you the issue...,
 // sometimes, even your IDE can tell you what's wrong
 
-// === SOLUTION ===
+// === PROBLEM ===
 // the problem with the function is that the the variable $x is undefined.
+// === SOLUTION ===
 // solution = it needs to be a parameter in the function because we pass value 2 when we call it.
 
 echo "Exercise 1 starts here:";
@@ -22,9 +23,13 @@ new_exercise(2);
 // === Exercise 2 ===
 // Below we create a week array with all days of the week.
 // We then try to print the first day which is monday, execute the code and see what happens.
+
+// === PROBLEM ===
+// the first item in an array always starts at index 0 so when we chose index 1, it will print tuesday
 // === SOLUTION ===
+// select index 0 to get monday
 
 $week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
-$monday = $week[1];
+$monday = $week[0];
 
 echo $monday;
